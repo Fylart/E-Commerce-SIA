19fd9e5ceac651ae50faea6a8469eeea19bad65d
Fylart <fylartthrowaway0@gmail.com>
2024-10-27 02:06:20 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Index.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="category">
+        <!-- ito yung pahaba na may picture saka text sa gitna -->
+        <div id="banner">
+            <img src="images/Placeholder.webp" alt="Background Image" class="background-image">
+            <h1 id="bannerHead">NEW<br>PRODUCT !</h1>
+        </div>
+
+
+        <!-- para to dun sa pinaka taas -->
+<!--         <div>
+            <h1>SHOP BY CATEGORY</h1>
+        </div> -->
+
+
+        <!-- ito isang buong container ng mga items, yung nasa loob yung each individual items -->
+        <div id="items">
+            <div class="item">
+                <a href="" class="soloPage">
+                    <img src="images/Placeholder.webp" alt="" class="itemImg">
+                    <h2>Balancing Body Oil</h2>                    
+                </a>
+                
+
+                <h3 class="price">₱499.00</h3>
+                <a href="" class="cart">ADD TO CART</a>
+            </div>
+
+            <div class="item">
+                <a href="" class="soloPage">
+                    <img src="images/Placeholder.webp" alt="" class="itemImg">
+                    <h2>Balancing Body Oil</h2>                    
+                </a>
+                
+
+                <h3 class="price">₱499.00</h3>
+                <a href="" class="cart">ADD TO CART</a>
+            </div>
+
+            <div class="item">
+                <a href="" class="soloPage">
+                    <img src="images/Placeholder.webp" alt="" class="itemImg">
+                    <h2>Balancing Body Oil</h2>                    
+                </a>
+                
+
+                <h3 class="price">₱499.00</h3>
+                <a href="" class="cart">ADD TO CART</a>
+            </div>
+
+            <div class="item">
+                <a href="" class="soloPage">
+                    <img src="images/Placeholder.webp" alt="" class="itemImg">
+                    <h2>Balancing Body Oil</h2>                    
+                </a>
+                
+
+                <h3 class="price">₱499.00</h3>
+                <a href="" class="cart">ADD TO CART</a>
+            </div>
+
+
+        </div>
+    </div>
+</body>
+</html>
