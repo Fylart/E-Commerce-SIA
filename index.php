1bc294fcbe8e2245210e9675a12a441c91475bef
KID <sandagakristian90@gmail.com>
2024-10-29 22:54:01 +0800

    index html goods!

--- index.php
+++ index.php
@@ -7,68 +7,8 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="category">
-        <!-- ito yung pahaba na may picture saka text sa gitna -->
-        <div id="banner">
-            <img src="images/Placeholder.webp" alt="Background Image" class="background-image">
-            <h1 id="bannerHead">NEW<br>PRODUCT !</h1>
-        </div>
-
-
-        <!-- para to dun sa pinaka taas -->
-<!--         <div>
-            <h1>SHOP BY CATEGORY</h1>
-        </div> -->
-
-
-        <!-- ito isang buong container ng mga items, yung nasa loob yung each individual items -->
-        <div id="items">
-            <div class="item">
-                <a href="" class="soloPage">
-                    <img src="images/Placeholder.webp" alt="" class="itemImg">
-                    <h2>Balancing Body Oil</h2>                    
-                </a>
-                
-
-                <h3 class="price">₱499.00</h3>
-                <a href="" class="cart">ADD TO CART</a>
-            </div>
-
-            <div class="item">
-                <a href="" class="soloPage">
-                    <img src="images/Placeholder.webp" alt="" class="itemImg">
-                    <h2>Balancing Body Oil</h2>                    
-                </a>
-                
-
-                <h3 class="price">₱499.00</h3>
-                <a href="" class="cart">ADD TO CART</a>
-            </div>
-
-            <div class="item">
-                <a href="" class="soloPage">
-                    <img src="images/Placeholder.webp" alt="" class="itemImg">
-                    <h2>Balancing Body Oil</h2>                    
-                </a>
-                
-
-                <h3 class="price">₱499.00</h3>
-                <a href="" class="cart">ADD TO CART</a>
-            </div>
-
-            <div class="item">
-                <a href="" class="soloPage">
-                    <img src="images/Placeholder.webp" alt="" class="itemImg">
-                    <h2>Balancing Body Oil</h2>                    
-                </a>
-                
-
-                <h3 class="price">₱499.00</h3>
-                <a href="" class="cart">ADD TO CART</a>
-            </div>
-
-
-        </div>
-    </div>
+    <?php
+    include("html/index.html");
+    ?>
 </body>
-</html>
+</html> 
