d189efab14ef33dd35e5894f33b18f885310453b
KID <sandagakristian90@gmail.com>
2024-11-05 00:41:51 +0800

    Homepage Updated!!!

---
+++ aboutus.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/aboutus.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/header.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include("html/header.html");
+    include("html/aboutUs.html");
+    include("html/footer.html");
+    ?>
+</body>
+</html> 
